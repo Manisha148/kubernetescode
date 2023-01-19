12a87b9d3a71ed287cb5a3e72de0356df0bc2559
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-01-19 17:15:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,7 @@
-# from flask import Flask
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#     return 'Manisha Verma '
-from flask import *
-import os
-import socket
-
+from flask import Flask
 app = Flask(__name__)
 
-# app.config["Debug"] = True
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template('index.html')
+def hello_world():
+    return 'Manisha Verma '
 
-    
-if _name_ == '__main__':
-    app.run()
-
-
-PORT = int(os.environ.get("PORT", 9090))
-if _name_ == '__main__':
-    app.run(threaded=True,host='0.0.0.0',port=PORT)
-
-if _name_ == "__main__":
-  app.run(host='0.0.0.0', port=9090)
